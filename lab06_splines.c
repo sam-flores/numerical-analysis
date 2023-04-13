8869baeea2bca16edfcedf780b15bae8056350c8
sam-flores <netusersam@gmail.com>
2023-04-13 11:57:40 -0700

    cleared some space

--- lab06_splines.c
+++ lab06_splines.c
@@ -115,7 +115,6 @@ int solve(double u[max_n][max_n], double v[max_n], int n){
 
 }
 
-
 void print_system(double u[max_n][max_n], double v[max_n], int n){
   for(int i = 0; i < n; i++){
     for(int j = 0; j < n; j++){
