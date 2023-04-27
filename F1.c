b8b189ee1bbbfdf3b9207210abf707ba4c60462b
sam-flores <netusersam@gmail.com>
2023-04-27 09:52:38 -0700

    added jeff files

---
+++ F1.c
@@ -0,0 +1,22 @@
+
+
+/* 
+
+Compute the limit of 
+
+
+ 17  +  [14 - 8*cos(x) + cos(2*x)] * cos(2*x)   - 24*cos(x)
+------------------------------------------------------------
+  [x*exp(x) - x] * [1 + 2*x*x - cos(2*x) - 4*x*sin(x)]
+
+
+as x approaches 0.
+
+The answer is pleasant.
+
+*/
+
+
+
+
+
