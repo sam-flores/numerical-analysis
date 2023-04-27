f202f21bcc6325b75365061a62cd8a82d5f4c18c
sam-flores <samuel@lclark.edu>
2023-04-27 12:59:50 -0700

    primitive zoom and graphing with splines working

--- graph.c
+++ graph.c
@@ -18,8 +18,8 @@ void solve_nx3_system(double v[], double x[], double y[], int n)
 	nx3_eliminate_lower_diagonal(lower, middle, upper, v, n);
 	nx3_eliminate_upper_diagonal(lower, middle, upper, v, n);
 	nx3_eliminate_middle_diagonal(lower, middle, upper, v, n);
-	printf("----------------------------solved-------------\n");
-	print_nx3_system(lower, middle, upper, v, n);
+	// printf("----------------------------solved-------------\n");
+	// print_nx3_system(lower, middle, upper, v, n);
 }
 
 double get_y(double x0, double x1, double y0, double y1, double A, double B, double t)
@@ -44,31 +44,37 @@ double f(double x, double t)
 {
 	double fx = 17 + (14 - 8 * cos(x) + cos(2 * x)) * cos(2 * x) - 24 * cos(x);
 	fx /= (x * exp(x) - x) * (1 + 2 * x * x - cos(2 * t) - 4 * x * sin(x));
-	return sin(x)*sin(t);
+	return exp(-x * x);
 }
 
-void draw_axes(double width, double height, double a, double b)
+void draw_axes(double width, double height,
+			   double a, double b,
+			   double c, double d)
 {
 	G_rgb(0, 0, 1);
 	double normalize = width / (b - a);
 	double middle = normalize * (a + b) / 2;
-	G_line(0, height / 2, width, height / 2);
+	double middle_y = normalize * (c + d) / 2;
+
+	G_line(0, height / 2 - middle_y, width, height / 2 - middle_y);
 	G_line(width / 2 - middle, height, width / 2 - middle, 0);
 }
 
-void graph(double (*f)(double x, double t), double a, double b,
-		  double width, double height, double t,
-		  double xp[], double yp[], int n)
+void graph(double (*f)(double x, double t),
+		   double a, double b, double c, double d,
+		   double width, double height,
+		   double t, double xp[], double yp[], int n)
 {
 	double x = a;
 	double delta = (b - a) / (double)n;
 	double normalize = width / (b - a);
-	double middle = normalize * (a + b) / 2;
+	double middle_x = normalize * (a + b) / 2;
+	double middle_y = normalize * (c + d) / 2;
 	G_rgb(0, 1, 0);
 	for (int i = 1; i <= n + 1; i++)
 	{
-		xp[i - 1] = x * normalize + width / 2 - middle;
-		yp[i - 1] = f(x, t) * normalize + height / 2;
+		xp[i - 1] = x * normalize + width / 2 - middle_x;
+		yp[i - 1] = f(x, t) * normalize + height / 2 - middle_y;
 		G_point(xp[i - 1], yp[i - 1]);
 		x += delta;
 	}
@@ -82,15 +88,17 @@ int main()
 	G_rgb(0, 0, 0);
 	G_clear();
 
-	double a = -1, b = 1;
+	double a = -10, b = 10;
+	double c = -10, d = 10;
 	int q = 0;
 	double t = 0;
 	double x[max_n], y[max_n], v[2 * max_n];
 	int n = 133;
+
 	while (q != 'q')
 	{
-		draw_axes(width, height, a, b);
-		graph(f, a, b, width, height, t, x, y, n - 1);
+		draw_axes(width, height, a, b, c, d);
+		graph(f, a, b, c, d, width, height, t, x, y, n - 1);
 		solve_nx3_system(v, x, y, n);
 		G_rgb(0, 1, 1);
 		draw_spline(x, y, v, n);
@@ -99,13 +107,13 @@ int main()
 		usleep(10000);
 		if (q == 65362)
 		{
-			a /= 2;
-			b /= 2;
+			c += 1;
+			d += 1;
 		}
 		else if (q == 65364)
 		{
-			a *= 2;
-			b *= 2;
+			c -= 1;
+			d -= 1;
 		}
 		else if (q == 65363)
 		{
@@ -116,6 +124,22 @@ int main()
 		{
 			a -= 1;
 			b -= 1;
+		}
+		else if (q == 115)
+		{
+
+			a *= 2;
+			b *= 2;
+			c *= 2;
+			d *= 2;
+		}
+		else if (q == 119)
+		{
+
+			a /= 2;
+			b /= 2;
+			c /= 2;
+			d /= 2;
 		}
 		G_rgb(0, 0, 0);
 		G_clear();
